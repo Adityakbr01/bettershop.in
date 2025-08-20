55f12be202b66a5439d5a4ce0705fec277c432b1
Adityakbr01 <aditykbr01@gmail.com>
2025-08-20 22:34:33 +0530

    fix: Update access token expiration comment for clarity and ensure correct maxAge conversion in cookie configuration

--- Server/src/configs/cookieConfig.ts
+++ Server/src/configs/cookieConfig.ts
@@ -1,9 +1,9 @@
 import { constant } from "@/configs/_Config";
 
-export const getCookieConfig = (maxAge = constant.ENV.ACCESSTOKEN_EXPIRES_IN) => ({ // 1 hour
+export const getCookieConfig = (maxAge = constant.ENV.ACCESSTOKEN_EXPIRES_IN) => ({
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: "strict" as const,
   path: "/",
-  maxAge
+  maxAge: maxAge * 1000 // maxAge in seconds * 1000 to convert to milliseconds
 });
